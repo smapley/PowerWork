1a5dfb2fca405bb1338f8d233cf6d441781928d4
smapley <smapley@163.com>
2015-11-16 23:10:51 +0800

    Revert "Smapley—branch"

--- app/src/main/java/com/smapley/powerwork/holder/Add_Text_Holder.java
+++ app/src/main/java/com/smapley/powerwork/holder/Add_Text_Holder.java
@@ -2,11 +2,7 @@ package com.smapley.powerwork.holder;
 
 import android.content.Context;
 import android.view.View;
-import android.widget.EditText;
 
-import com.lidroid.xutils.util.LogUtils;
-import com.smapley.powerwork.R;
-import com.smapley.powerwork.adapter.AddTaskAdapter;
 import com.smapley.powerwork.mode.Add_Text_Mode;
 
 /**
@@ -14,26 +10,13 @@ import com.smapley.powerwork.mode.Add_Text_Mode;
  */
 public class Add_Text_Holder extends BaseHolder {
 
-    private EditText add_et_text;
 
     public Add_Text_Holder(View view) {
         super(view);
-        add_et_text = (EditText) view.findViewById(R.id.add_et_text);
     }
 
 
-    public void setData(final AddTaskAdapter adapter, final Add_Text_Mode mode, final int position) {
-        add_et_text.setText(mode.getText());
-        add_et_text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                mode.setText(add_et_text.getText().toString());
-                if (b) {
-                    adapter.focusPosition = position;
-                    LogUtils.i("asdf" + position + add_et_text.getSelectionStart());
-                }
-            }
-        });
+    public void setData(Context context, Add_Text_Mode mode) {
 
     }
 }
